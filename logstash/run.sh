6ce575625d6021ea5c2581e528e7584dcae9bb48
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-11-14 11:57:39 -0600

    Paramaterize logstash. config xpack

--- logstash/run.sh
+++ logstash/run.sh
@@ -11,8 +11,12 @@ docker_version=$(cat VERSION)
 docker run --rm -d \
     -p 9400:9400 \
     -p 5044:5044 \
-    -p 443:443 \
     -p 9600:9600 \
+    -e xpack.monitoring.elasticsearch.username='${ELASTIC_USERNAME}' \
+    -e xpack.monitoring.elasticsearch.password='${ELASTIC_PASSWORD}' \
+    -e xpack.monitoring.elasticsearch.url="https://elk.core.merrillcorp.com:443/elastic" \
+    -e ELASTIC_PASSWORD="${ELASTIC_PASSWORD}" \
+    -e ELASTIC_HOST="https://elk.core.merrillcorp.com:443/elastic" \
     --name mrll-bot-logstash \
     -v "${__dir}/pipeline/.:/usr/share/logstash/pipeline/" \
     "mrllsvc/docker-logstash:${docker_version}"
