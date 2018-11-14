191dca39800bdf19e8c997df18737f5e4d6585e2
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-11-14 14:54:29 -0600

    configure xpack across stack

--- kibana/run.sh
+++ kibana/run.sh
@@ -12,6 +12,7 @@ docker_version=$(cat VERSION)
 docker run --rm -d \
     -p 5601:5601 \
     -e ELASTIC_PASSWORD="${ELASTIC_PASSWORD}" \
+    -e ELASTIC_URL="https://elk.core.merrillcorp.com:443/elastic" \
     -v "$(pwd)/kibana.yml:/usr/share/kibana/config/kibana.yml" \
     --name mrll-bot-kibana \
     "mrllsvc/docker-kibana:${docker_version}"
--- logstash/run.sh
+++ logstash/run.sh
@@ -12,9 +12,9 @@ docker run --rm -d \
     -p 9400:9400 \
     -p 5044:5044 \
     -p 9600:9600 \
-    -e xpack.monitoring.elasticsearch.username='${ELASTIC_USERNAME}' \
-    -e xpack.monitoring.elasticsearch.password='${ELASTIC_PASSWORD}' \
-    -e xpack.monitoring.elasticsearch.url="https://elk.core.merrillcorp.com:443/elastic" \
+    -p 444:443 \
+    -e xpack.monitoring.elasticsearch.username=logstash_system \
+    -e xpack.monitoring.elasticsearch.password='${ELASTIC_MONITORING_PASSWORD}' \
     -e ELASTIC_PASSWORD="${ELASTIC_PASSWORD}" \
     -e ELASTIC_HOST="https://elk.core.merrillcorp.com:443/elastic" \
     --name mrll-bot-logstash \
